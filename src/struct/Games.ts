8bf43be84a63eddb92b1e68e63f06a95d7ad8298
Mateie <mateie@email.com>
2024-03-21 23:15:07 -0700

    not bringing back steam anytime soon

--- src/struct/Games.ts
+++ src/struct/Games.ts
@@ -1,5 +1,4 @@
 import { LegacyClient as OsuApi } from "osu-web.js";
-//import SteamAPI from "steamapi";
 import Valorant from "./games/valorant";
 import Warframe from "./games/Warframe";
 
@@ -7,15 +6,11 @@ const { OSU_API, STEAM_API } = process.env;
 
 export default class Games {
     readonly osu: OsuApi;
-    // readonly steam: SteamAPI;
     readonly valorant: Valorant;
     readonly warframe: Warframe;
 
-    // TODO: Bring back SteamAPI when it gets fixed for Yarn or Bun
-
     constructor() {
         this.osu = new OsuApi(OSU_API ?? "");
-        //this.steam = new SteamAPI(STEAM_API ?? "");
         this.valorant = new Valorant();
         this.warframe = new Warframe();
     }
