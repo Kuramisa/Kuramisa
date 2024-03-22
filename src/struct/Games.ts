f40385bfdfc3844cbacd695ccb68780875941eba
Mateie <mateie@email.com>
2024-03-22 02:28:22 -0700

    Removed warframe

--- src/struct/Games.ts
+++ src/struct/Games.ts
@@ -1,17 +1,14 @@
 import { LegacyClient as OsuApi } from "osu-web.js";
 import Valorant from "./games/valorant";
-import Warframe from "./games/Warframe";
 
-const { OSU_API, STEAM_API } = process.env;
+const { OSU_API } = process.env;
 
 export default class Games {
     readonly osu: OsuApi;
     readonly valorant: Valorant;
-    readonly warframe: Warframe;
 
     constructor() {
         this.osu = new OsuApi(OSU_API ?? "");
         this.valorant = new Valorant();
-        this.warframe = new Warframe();
     }
 }
