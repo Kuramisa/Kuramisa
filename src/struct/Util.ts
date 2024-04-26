87ef276ca96e2dc61dd37eea624efac8b39a212a
Mateie <mateie@email.com>
2024-04-26 00:02:03 -0700

    removed abbrev func

--- src/struct/Util.ts
+++ src/struct/Util.ts
@@ -52,27 +52,6 @@ export default class KUtil {
         this.pagination = new UtilPagination();
     }
 
-    abbrev(num: any) {
-        if (!num || isNaN(num)) return 0;
-        if (typeof num === "string") num = parseInt(num);
-        const decPlaces = Math.pow(10, 1);
-
-        const abbrev = ["K", "M", "B", "T"];
-        for (let i = abbrev.length - 1; i >= 0; i--) {
-            const size = Math.pow(10, (i + 1) * 3);
-            if (size <= num) {
-                num = Math.round((num * decPlaces) / size) / decPlaces;
-                if (num === 1000 && i < abbrev.length - 1) {
-                    num = 1;
-                    i++;
-                }
-                num += abbrev[i];
-                break;
-            }
-        }
-        return num;
-    }
-
     chunk(arr: any, size: number) {
         const temp = [];
         for (let i = 0; i < arr.length; i += size) {
