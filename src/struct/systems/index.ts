3bb6084412bfc19faca9ff86f8b251d250cc58f7
Mateie <mateie@email.com>
2024-06-22 17:39:19 -0700

    Removed RSS since its not really maintanable

--- src/struct/systems/index.ts
+++ src/struct/systems/index.ts
@@ -3,20 +3,17 @@ import Cryptr from "cryptr";
 import OpenAI from "./OpenAI";
 import Music from "./Music";
 import Kuramisa from "Kuramisa";
-import RSS from "./RSS";
 
 const { CRYPT_SECRET } = process.env;
 
 export default class KSystems {
     readonly crypt: Cryptr;
     readonly openai: OpenAI;
-    readonly rss: RSS;
     readonly music: Music;
 
     constructor(kuramisa: Kuramisa) {
         this.crypt = new Cryptr(CRYPT_SECRET ?? "");
         this.openai = new OpenAI();
-        this.rss = new RSS();
         this.music = new Music(kuramisa);
     }
 }
