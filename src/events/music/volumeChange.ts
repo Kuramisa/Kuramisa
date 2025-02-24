8b2a3ee182f3109466d0f94f667fa1fcf7a71076
Mateie <contact@mateie.dev>
2025-02-23 16:18:33 -0800

    fixed volume change to be better

--- src/events/music/volumeChange.ts
+++ src/events/music/volumeChange.ts
@@ -1,8 +1,7 @@
-import { AbstractEvent, Event } from "classes/Event";
 import kuramisa from "@kuramisa";
-import { Embed } from "@builders";
+import { AbstractEvent, Event } from "classes/Event";
 import { GuildQueue } from "discord-player";
-import type { GuildTextBasedChannel } from "discord.js";
+import { EmbedBuilder, type GuildTextBasedChannel } from "discord.js";
 
 @Event({
     event: "volumeChange",
@@ -25,17 +24,17 @@ export default class VolumeChangeEvent extends AbstractEvent {
         if (!guild.musicMessage) return;
 
         const plainEmbed = guild.musicMessage.embeds[0];
-        const embed = Embed.from(plainEmbed);
+        const embed = EmbedBuilder.from(plainEmbed);
         if (!plainEmbed.description) return;
-
-        const oldVolumeText = `${music.volumeEmoji(oldVolume)} **Volume**: ${oldVolume}%`;
-        const newVolumeText = `${music.volumeEmoji(newVolume)} **Volume**: ${newVolume}%`;
 
         guild.musicMessage.edit({
             content: "",
             embeds: [
                 embed.setDescription(
-                    plainEmbed.description.replace(oldVolumeText, newVolumeText)
+                    plainEmbed.description.replaceAll(
+                        oldVolume.toString(),
+                        newVolume.toString()
+                    )
                 ),
             ],
             components: music.playerControls(),
